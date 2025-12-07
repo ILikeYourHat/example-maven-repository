bc42de81ff8f839233fc278b2b6a3fe1bfa9fa03
Marcin <marcin.laskowski.07@gmail.com>
2025-12-07 14:12:54 +0100

    Create zip build step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,32 +1,10 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
 version = "2025.11"
 
 project {
-
     buildType(Build)
 }
 
@@ -39,9 +17,16 @@ object Build : BuildType({
 
     steps {
         maven {
+            name = "Generate JavaDocs"
             id = "Maven2"
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            goals = "javadoc:javadoc"
+        }
+        script {
+            name = "Zip JavaDocs"
+            id = "Zip"
+            scriptContent = """
+                zip -r ciOutput/apidocs.zip target/reports/apidocs
+            """.trimIndent()
         }
     }
 })
