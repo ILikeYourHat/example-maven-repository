e2032ca4f66f3c164c8cd25056d98f0503b6ff46
Marcin <marcin.laskowski.07@gmail.com>
2025-12-08 19:49:52 +0100

    Add another workflow for scheduling page sync

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,12 +1,15 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.notifications
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.ui.add
 
 version = "2025.11"
 
 project {
     buildType(Build)
+    buildType(SyncReleaseNotes)
 }
 
 object Build : BuildType({
@@ -36,4 +39,35 @@ object Build : BuildType({
     }
 
     artifactRules = "+:target/apidocs.zip"
+})
+
+object SyncReleaseNotes : BuildType({
+    name = "Sync release notes"
+
+    val releaseNotesPage = "https://docs.gradle.org/9.2.1/release-notes.html"
+
+    triggers {
+        schedule {
+            schedulingPolicy = daily {
+                hour = 6
+            }
+            triggerBuild = always()
+        }
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "Download release notes"
+            scriptContent = """
+                mkdir releaseNotes
+                wget -P releaseNotes --page-requisites --convert-links --no-parent "$releaseNotesPage"
+            """.trimIndent()
+        }
+    }
+
+    artifactRules = "+:releaseNotes"
 })
