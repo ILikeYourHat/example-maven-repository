ccb0de6b2379dfee432ff20e6411970ebee8371a
Marcin <marcin.laskowski.07@gmail.com>
2025-12-09 17:25:03 +0100

    Add a valid git config

--- sync_release_notes.sh
+++ sync_release_notes.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# Configure git settings for this repo
+git config user.email "someone@example.com"
+git config user.name "TeamCity server"
+
 git add releaseNotes
+
 # Check if there is anything to commit
 if ! git diff --cached --quiet; then
   git commit -m "Automatic sync: updated release notes"
