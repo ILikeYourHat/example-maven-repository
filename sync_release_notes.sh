713c101df6e43bc58864e4abcddac656ae6c9678
Marcin <marcin.laskowski.07@gmail.com>
2025-12-09 17:43:50 +0100

    Add remote as ssh resource

--- sync_release_notes.sh
+++ sync_release_notes.sh
@@ -3,7 +3,10 @@
 # Configure git settings for this repo
 git config user.email "someone@example.com"
 git config user.name "TeamCity server"
+# Be sure that remote points out to repository trough the ssh
+git remote set-url --push origin 'git@github.com:ILikeYourHat/example-maven-repository.git'
 
+# Add all files from the target folder
 git add releaseNotes
 
 # Check if there is anything to commit
